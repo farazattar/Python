afecb32af68008074a5496ee7440c104df8a6893
farazattar1985 <farazattar1985@gmail.com>
2021-03-19 23:46:08 -0700

    Growth Tracker-inital commit

---
+++ growth_tracker.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+mounth=[0, 1,2,3]
+weight=[2.5, 3,5,7]
+# fig=plt.figure()
+# ax = fig.add_subplot(111)
+# ax.plot(mounth, weight, color='lightblue', linewidth=3)
+plt.plot(mounth, weight, color='lightblue', linewidth=3)
+plt.show()
---
+++ io.py
@@ -0,0 +1,12 @@
+# Python scripts: This program only test input/outputs.
+import matplotlib as mpl
+
+print('Hello World!')
+print('Please Type Your Name')
+yourName=input()
+print('Your Name is '+yourName+'.')
+print('The Length of your name is:')
+print(len(yourName))
+print('What is your age?')
+yourAge=input()
+print('Your will be '+str(int(yourAge)+1)+ ' in a year.')
