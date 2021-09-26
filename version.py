e4051f9e9639938469f43d85bda233c8025ad1f4
farazattar <63309056+farazattar@users.noreply.github.com>
2021-09-26 10:29:59 +0330

    Print the version of Python's Libraries
    Print the version of Python's Libraries: Numpy, Scipy, Pandas, ... .

---
+++ version.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import scipy as sp
+import seaborn as sb
+
+print('Numpy version is {} '.format(np.__version__))
+print('Pandas version is {} '.format(pd.__version__))
+print('Scipy version is {} '.format(sp.__version__))
+print('Seaborn version is {} '.format(sb.__version__))
